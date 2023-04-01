2cca925fcbd07b6458f044b4e4febfcdda2ed397
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2023-04-01 19:06:29 +0200

    Update download.py

--- download.py
+++ download.py
@@ -9,8 +9,7 @@ def create_TRE_from_Template(textToReplace):
                 '/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'CodeSystem-'+textToReplace+'.download.html')
     for line in fileinput.input('/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'CodeSystem-'+textToReplace+'.download.html',
                                 inplace=True):
-        print(line.replace('header_to_replace', textToReplace), end='')
-        print(line.replace('to_replace', textToReplace.replace("TRE-","TRE_")), end='')
+        print(line.replace('header_to_replace', textToReplace).replace('to_replace', textToReplace.replace("TRE-","TRE_")), end='')
         
 
 def create_JDV_from_Template(textToReplace):
@@ -18,8 +17,8 @@ def create_JDV_from_Template(textToReplace):
                 '/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'ValueSet-'+textToReplace+'.download.html')
     for line in fileinput.input('/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'ValueSet-'+textToReplace+'.download.html',
                                 inplace=True):
-        print(line.replace('header_to_replace', textToReplace), end='')
-        print(line.replace('to_replace', textToReplace.replace("JDV-","JDV_")), end='')
+        print(line.replace('header_to_replace', textToReplace), end='').replace('to_replace', textToReplace.replace("JDV-","JDV_"))
+
         
 
 
