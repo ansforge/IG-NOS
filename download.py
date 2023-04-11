ae3eb7223d4f333ae91dcfd9f5a68f6f97534f75
Mael PRIOUR <101335975+M-Priour@users.noreply.github.com>
2023-04-11 20:45:55 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,21 +1,21 @@
 import os,re,shutil
 import fileinput
 
-dir_path = '/home/runner/work/IG-NOS/IG-NOS/temp/pages/'
+dir_path = './temp/pages/'
 
 # boucler sur toutes les pages
 def create_TRE_from_Template(textToReplace):
     shutil.copy('./template_nos/includes/template.download.html',
-                '/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'CodeSystem-'+textToReplace+'.download.html')
-    for line in fileinput.input('/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'CodeSystem-'+textToReplace+'.download.html',
+                './temp/pages/'+'CodeSystem-'+textToReplace+'.download.html')
+    for line in fileinput.input('./temp/pages/'+'CodeSystem-'+textToReplace+'.download.html',
                                 inplace=True):
         print(line.replace('header_to_replace', textToReplace).replace('to_replace', textToReplace.replace("TRE-","TRE_")), end='')
         
 
 def create_JDV_from_Template(textToReplace):
     shutil.copy('./template_nos/includes/template.download.html',
-                '/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'ValueSet-'+textToReplace+'.download.html')
-    for line in fileinput.input('/home/runner/work/IG-NOS/IG-NOS/temp/pages/'+'ValueSet-'+textToReplace+'.download.html',
+                './temp/pages/'+'ValueSet-'+textToReplace+'.download.html')
+    for line in fileinput.input('./temp/pages/'+'ValueSet-'+textToReplace+'.download.html',
                                 inplace=True):
         print(line.replace('header_to_replace', textToReplace).replace('to_replace', textToReplace.replace("JDV-","JDV_")).replace('CodeSystem', 'ValueSet'), end='')
 
