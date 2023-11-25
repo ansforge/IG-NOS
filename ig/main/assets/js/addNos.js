7c5ed87c677990da45da93340a914808c2ad63e3
M-Priour <M-Priour@users.noreply.github.com>
2023-11-25 17:27:07 +0000

    deploy: f75838ffb7d841c3371c945f0f605315d8e316f4

--- ig/main/assets/js/addNos.js
+++ ig/main/assets/js/addNos.js
@@ -17,4 +17,42 @@ $(document).ready(function(){
 
         $('#back-to-top').tooltip('show');
 
+$('#orig').find('table').each(function(indextable) { 
+
+	$('<div class="form-group pull-right"> <input type="text"  style="height:auto;font-size:12px" class="search' + indextable +' form-control" placeholder="Recherche">  <span class="counter' + indextable + ' "></span></div>').insertBefore($(this));	
+	firstTr = $(this).find('tr:first').remove()
+	firstTr.find('td').contents().unwrap().wrap('<th>')
+	$(this).prepend($('<thead></thead>').append(firstTr))
+	$(this).addClass("results"+indextable); 
+
+  $(".search"+indextable).keyup(function () {
+    var searchTerm = $(".search"+indextable).val();
+    var listItem = $('.results'+indextable +' tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+    
+  $(".results"+indextable +" tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results"+indextable +" tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results'+indextable +' tbody tr[visible="true"]').length;
+    $('.counter'+indextable).text(jobCount + ' item');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+		  });
+	
+});
+
+
+
+     
 });
