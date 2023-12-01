a5156cc95e638ab0cc4ac3c1ce9983f65c9c6916
M-Priour <M-Priour@users.noreply.github.com>
2023-12-01 08:27:49 +0000

    deploy: efcbffb488263a0a0c962fb84e7967f119378ae0

--- ig/main/assets/js/addNos.js
+++ ig/main/assets/js/addNos.js
@@ -41,8 +41,6 @@ $(document).ready(function(){
         $('#back-to-top').tooltip('show');
 
 
-if($("table.codes").find('tr:eq(0) th:eq(4)').text()=='dateFin')
-	$("table.codes tr td:nth-child(5):not(:empty)").parent().children().css("background-color","#E69215");
 
 
 $('#orig').find('table.codes').each(function(indextable) { 
@@ -86,5 +84,7 @@ $('#orig').find('table.codes').each(function(indextable) {
 	
 });
 
-     
+
+if($("table.codes").find('tr:eq(0) th:eq(4)').text()=='dateFin')
+	$("table.codes tr td:nth-child(5):not(:empty)").parent().children().css("background-color","#E69215");     
 });
